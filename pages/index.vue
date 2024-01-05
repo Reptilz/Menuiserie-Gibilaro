9cb43dab80d2008bd449ebe5c9671d2bf5acaa04
Reptilz <jmorlet94@gmail.com>
2024-01-05 19:41:26 +0100

    responsive mobile index

--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,20 @@ useHead({
 </script>
 
 <template>
-    <div class="bg-black h-screen flex flex-col items-center justify-center">
-        <div class="text-center">
-            <h1 class="text-white text-5xl font-bold mb-4">Site en construction</h1>
-            <p class="text-yellow text-xl">Nous sommes en train de peaufiner les détails. Revenez bientôt pour découvrir notre nouveau site !</p>
-            <img src="~/assets/img/waiting.gif" alt="Image de construction" class="img-construct max-w-md mx-auto"/>
-        </div>
-
-        <!-- <audio controls autoplay loop class="music">
-                <source src="~/assets/audio/DjadjaDinaz.mp4" type="audio/mp4">
-                Votre navigateur ne supporte pas l'élément audio.
-        </audio> -->
+    <div class="bg-black min-h-screen flex flex-col items-center justify-center p-4">
+      <div class="text-center">
+        <h1 class="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 mx-8">Site en construction</h1>
+        <p class="text-yellow text-base sm:text-xl md:text-xl lg:text-2xl mx-12">Nous sommes en train de peaufiner les détails. Revenez bientôt pour découvrir notre nouveau site !</p>
+        <img src="~/assets/img/waiting.gif" alt="Image de construction" class="img-construct max-w-md mx-auto mt-4"/>
+      </div>
+  
+      <!-- <audio controls autoplay loop class="music">
+            <source src="~/assets/audio/DjadjaDinaz.mp4" type="audio/mp4">
+            Votre navigateur ne supporte pas l'élément audio.
+      </audio> -->
     </div>
-</template>
+  </template>
+  
 
 <style lang="scss">
 .img-construct {
