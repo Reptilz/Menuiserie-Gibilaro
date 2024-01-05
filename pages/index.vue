b3a8008e83f81ab6b859cef05778fc8c9d3f3437
Reptilz <jmorlet94@gmail.com>
2024-01-05 17:49:58 +0100

    edit style in index

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@ useHead({
 <template>
     <div class="bg-black h-screen flex flex-col items-center justify-center">
         <div class="text-center">
-            <h1 class="text-white text-5xl font-bold mb-2">Site en construction</h1>
+            <h1 class="text-white text-5xl font-bold mb-4">Site en construction</h1>
             <p class="text-yellow text-xl">Nous sommes en train de peaufiner les détails. Revenez bientôt pour découvrir notre nouveau site !</p>
             <img src="~/assets/img/waiting.gif" alt="Image de construction" class="img-construct max-w-md mx-auto"/>
         </div>
