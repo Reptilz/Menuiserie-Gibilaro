85d2c4846a859ac1d01cf89834ffe6469500431b
Reptilz <jmorlet94@gmail.com>
2024-01-11 22:34:08 +0100

    edit logo menu hamburger on mobile

--- components/Header/Nav/index.vue
+++ components/Header/Nav/index.vue
@@ -14,20 +14,7 @@
             @click="toggleMobileMenu"
             class="text-white focus:outline-none mr-8"
           >
-            <svg
-              class="lg:hidden h-10 w-10"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
+          <i class="fa-solid fa-bars menu-hamburger-logo text-3xl"></i>
           </button>
         </div>
 
@@ -106,4 +93,9 @@ const toggleMobileMenu = () => {
 </script>
 
 <style scoped>
+.menu-hamburger-logo {
+  font-size: 2rem;
+  margin-bottom: 15px;
+  margin-right: 10px;
+}
 </style>
