1c0a40383c894f3ced70a5a749805bf178dc87ca
Reptilz <jmorlet94@gmail.com>
2024-01-18 19:40:11 +0100

    fix footer position

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,3 +5,26 @@
         <Footer />
     </div>
 </template>
+
+<style lang="scss">
+.defaultLayout {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+slot {
+  flex-grow: 1; 
+}
+
+
+@media (max-width: 768px) {
+  .defaultLayout {
+    position: relative;
+  }
+
+  slot {
+    margin-bottom: 80px;  
+  }
+}
+</style>
