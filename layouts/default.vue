b5b3c42ab2c47510df67dd2f3e8652c536928c1c
Reptilz <jmorlet94@gmail.com>
2024-01-18 19:30:52 +0100

    fix footer position

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,7 @@
 <template>
-    <div class="defaultLayout">
-      <div class="content">
-        <Header />
-        <slot />
-      </div>
-      <Footer />
-    </div>
-  </template>
-  
-  <style lang="scss">
-  .defaultLayout {
-    display: grid;
-    grid-template-rows: 1fr auto; 
-    min-height: 100vh; 
-  }
-  
-  .content {
-    grid-row: 1;
-  }
-  </style>
-  
+  <div class="flex flex-col min-h-screen">
+    <Header />
+    <slot class="flex-grow" />
+    <Footer />
+  </div>
+</template>
