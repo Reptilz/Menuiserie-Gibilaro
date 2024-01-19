3be6c1311bfe10fc6b3f447d335ce85abb29921d
Reptilz <jmorlet94@gmail.com>
2024-01-19 15:47:53 +0100

    add page devis + nav

--- components/Header/Nav/index.vue
+++ components/Header/Nav/index.vue
@@ -2,84 +2,54 @@
   <nav class="bg-black p-0 border-b-2 border-yellow">
     <div class="container mx-auto">
       <div class="flex items-center justify-between">
-        <img
-          src="~/assets/img/logo/Transparent-Logo.png"
-          alt="Menuiserie Gibilaro logo"
-          class="w-56"
-        />
+        <img src="~/assets/img/logo/Transparent-Logo.png" alt="Menuiserie Gibilaro logo" class="w-56" />
 
         <!-- Burger Menu for Mobile -->
         <div v-if="!mobileMenuOpen" class="lg:hidden">
-          <button
-            @click="toggleMobileMenu"
-            class="text-white focus:outline-none mr-8"
-          >
+          <button @click="toggleMobileMenu" class="text-white focus:outline-none mr-8">
             <i class="fa-solid fa-bars menu-hamburger-logo text-3xl"></i>
           </button>
         </div>
 
         <!-- Mobile Menu -->
-        <div
-          v-show="mobileMenuOpen"
-          :class="{ 'mobile-menu-open': mobileMenuOpen }"
-          class="bg-black lg:hidden absolute inset-0 bg-gray-800 bg-opacity-100 z-50 origin-right"
-        >
+        <div v-show="mobileMenuOpen" :class="{ 'mobile-menu-open': mobileMenuOpen }"
+          class="bg-black lg:hidden absolute inset-0 bg-gray-800 bg-opacity-100 z-50 origin-right">
           <div class="flex justify-end pr-8 pt-8">
-            <button
-              @click="toggleMobileMenu"
-              class="text-white focus:outline-none"
-            >
-              <svg
-                class="h-10 w-10"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
+            <button @click="toggleMobileMenu" class="text-white focus:outline-none">
+              <svg class="h-10 w-10" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
               </svg>
             </button>
           </div>
 
           <div class="flex flex-col">
-            <NuxtLink to="/" @click="toggleMobileMenu"  class="mx-auto w-64">
+            <NuxtLink to="/" @click="toggleMobileMenu" class="mx-auto w-64">
               <img src="~/assets/img/logo/Transparent-Logo.png" />
             </NuxtLink>
-            <NuxtLink
-              to="/"
-              @click="toggleMobileMenu" 
-              class="text-white bg-blackLight bg-opacity-30 text-xl mt-3 p-5 hover:bg-yellow flex justify-between items-center"
-            >
+            <NuxtLink to="/" @click="toggleMobileMenu"
+              class="text-white bg-blackLight bg-opacity-30 text-xl mt-3 p-5 hover:bg-yellow flex justify-between items-center">
               Accueil
               <i class="fa-solid fa-chevron-right"></i>
             </NuxtLink>
-            <NuxtLink
-              to="/a-propos"
-              @click="toggleMobileMenu" 
-              class="text-white bg-blackLight bg-opacity-30 text-xl mt-3 p-5 hover:bg-yellow flex justify-between items-center"
-            >
-              À propos
-              <i class="fa-solid fa-chevron-right"></i>
-            </NuxtLink>
-            <NuxtLink
-              to="/realisations"
-              @click="toggleMobileMenu" 
-              class="text-white bg-blackLight bg-opacity-30 text-xl mt-3 p-5 hover:bg-yellow flex justify-between items-center"
-            >
+            <NuxtLink to="/realisations" @click="toggleMobileMenu"
+              class="text-white bg-blackLight bg-opacity-30 text-xl mt-3 p-5 hover:bg-yellow flex justify-between items-center">
               Nos réalisations
               <i class="fa-solid fa-chevron-right"></i>
             </NuxtLink>
-            <NuxtLink
-              to="/contact"
-              @click="toggleMobileMenu" 
-              class="text-white bg-blackLight bg-opacity-30 text-xl mt-3 p-5 hover:bg-yellow flex justify-between items-center"
-            >
+            <NuxtLink to="/a-propos" @click="toggleMobileMenu"
+              class="text-white bg-blackLight bg-opacity-30 text-xl mt-3 p-5 hover:bg-yellow flex justify-between items-center">
+              À propos
+              <i class="fa-solid fa-chevron-right"></i>
+            </NuxtLink>
+            <NuxtLink to="/contact" @click="toggleMobileMenu"
+              class="text-white bg-blackLight bg-opacity-30 text-xl mt-3 p-5 hover:bg-yellow flex justify-between items-center">
               Contact
+              <i class="fa-solid fa-chevron-right"></i>
+            </NuxtLink>
+            <NuxtLink to="/devis" @click="toggleMobileMenu"
+              class="text-black bg-yellow text-xl font-semibold mt-3 p-5 hover:bg-black hover:text-yellow hover:border flex justify-between items-center">
+              Devis gratuit
               <i class="fa-solid fa-chevron-right"></i>
             </NuxtLink>
           </div>
@@ -87,30 +57,22 @@
         </div>
 
         <!-- Regular Menu for Desktop - Hidden on Mobile -->
-        <div
-          class="hidden lg:flex lg:flex-grow justify-center xl:text-xl space-x-12"
-        >
-          <NuxtLink to="/" class="text-white hover:text-gray-300">
+        <div class="hidden lg:flex lg:flex-grow justify-center items-center xl:text-xl space-x-12">
+          <NuxtLink to="/" class="text-white hover:text-yellow">
             Accueil
           </NuxtLink>
-          <NuxtLink to="/a-propos" class="text-white hover:text-gray-300">
-            À propos
-          </NuxtLink>
-          <NuxtLink to="/realisations" class="text-white hover:text-gray-300">
+          <NuxtLink to="/realisations" class="text-white hover:text-yellow">
             Nos réalisations
           </NuxtLink>
-          <NuxtLink to="/contact" class="text-white hover:text-gray-300">
+          <NuxtLink to="/a-propos" class="text-white hover:text-yellow">
+            À propos
+          </NuxtLink>
+          <NuxtLink to="/contact" class="text-white hover:text-yellow">
             Contact
           </NuxtLink>
-        </div>
-
-        <!-- Phone zone - Hidden on Mobile -->
-        <div class="hidden lg:flex items-center space-x-2 xl:text-xl">
-          <i class="fas fa-phone-alt text-white"></i>
-          <a href="tel:+32498800504" class="text-white">+32 498 80 05 04</a>
-          <span class="text-xs text-gray-300 pr-12 xl:pr-0"
-            >(Appelez-nous)</span
-          >
+          <NuxtLink to="/devis" class="nav-devis text-black bg-yellow p-2 font-semibold hover:bg-black hover:text-yellow hover:border">
+            Devis gratuit
+          </NuxtLink>
         </div>
       </div>
     </div>
@@ -129,9 +91,11 @@ const toggleMobileMenu = () => {
 </script>
 
 <style scoped>
+/** Mobile menu **/
 .mobile-menu-open {
   max-height: 100vh;
 }
+
 .menu-hamburger-logo {
   font-size: 2rem;
   margin-bottom: 15px;
---
+++ pages/devis/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-black flex flex-col items-center justify-center">
+        <div class="text-center">
+            <h1 class="text-white text-4xl font-bold mb-4 mt-24 mx-4">Page en construction</h1>
+            <p class="text-yellow text-xl">Demander un devis gratuitement</p>
+            <img src="~/assets/img/waiting.gif" alt="Image de construction" class="img-construct mx-auto"/>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.img-construct {
+    position: relative;
+    bottom: 80px;
+}
+</style>
